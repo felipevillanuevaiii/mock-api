625e38ca7d35cb625006e70ee2ee3daf536e215a
felipevillanuevaiii <felipe.villanueva@fastek.ph>
2025-07-11 10:47:27 +0800

    test

--- middleware.js
+++ middleware.js
@@ -12,10 +12,16 @@ const readJsonFile = (filePath, folder) => {
 }
 
 module.exports = (req, res, next) => {
-    if (req.method) {
-        const file = readJsonFile('config.json', 'config');
-        const data = file.find(object => (object.path.startsWith(req.path) && object.method === req.method));
-        return res.status(data.code).json(readJsonFile(data.file, 'responses'));
+    try {
+        if (req.method) {
+            const file = readJsonFile('config.json', 'config');
+            const data = file.find(object => ((object.path === req.path) && object.method === req.method));
+            return res.status(data.code).json(readJsonFile(data.file, 'responses'));
+        }
+    } catch(error) {
+        return res.status(500).json({
+            message: "invalid url"
+        });
     }
     next();
 };
