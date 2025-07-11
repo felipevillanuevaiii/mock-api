9c5ddc990b49798b064cbf029df162087dace388
felipevillanuevaiii <felipe.villanueva@fastek.ph>
2025-07-11 09:42:31 +0800

    1st commit

---
+++ middleware.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const readJsonFile = (filePath, folder) => {
+    try {
+        const fullPath = path.join(__dirname, folder, filePath);
+        const data = fs.readFileSync(fullPath, 'utf8');
+        return JSON.parse(data);
+    } catch(error) {
+        console.error(`Error reading JSON file: ${filePath}`, error);
+    }
+}
+
+module.exports = (req, res, next) => {
+    if (req.method) {
+        const file = readJsonFile('config.json', 'config');
+        const data = file.find(object => (object.path.startsWith(req.path) && object.method === req.method));
+        return res.status(data.code).json(readJsonFile(data.file, 'responses'));
+    }
+    next();
+};
