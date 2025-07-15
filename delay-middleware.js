164fd4f39f271c6f5c7ce3047d253243dafdffa1
felipevillanuevaiii <felipe.villanueva@fastek.ph>
2025-07-15 10:08:32 +0800

    change delay middleware condition

--- delay-middleware.js
+++ delay-middleware.js
@@ -1,7 +1,7 @@
 
 module.exports = (req, res, next) => {
     try {
-        if (req.path.startsWith("/timeout")) {
+        if (req.path.endsWith("timeout")) {
             return;
         }
     } catch(error) {
