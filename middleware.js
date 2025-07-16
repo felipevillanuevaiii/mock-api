79ddd76972dbc2877a389c8df57c90977224f502
felipevillanuevaiii <felipe.villanueva@fastek.ph>
2025-07-16 08:22:55 +0800

    modify to cater query parameters

--- middleware.js
+++ middleware.js
@@ -9,16 +9,50 @@ const readJsonFile = (filePath, folder) => {
     } catch(error) {
         console.error(`Error reading JSON file: ${filePath}`, error);
     }
-}
+};
+
+const readTemplateFile = (filePath, folder) => {
+    try {
+        const fullPath = path.join(__dirname, folder, filePath);
+        return fs.readFileSync(fullPath, 'utf8');
+    } catch(error) {
+        console.error(`Error reading template file: ${filePath}`, error);
+    }
+};
 
 module.exports = (req, res, next) => {
     try {
         if (req.method) {
-            const file = readJsonFile('config.json', 'config');
-            const data = file.find(object => ((object.path === req.path) && object.method === req.method));
-            return res.status(data.code).json(readJsonFile(data.file, 'responses'));
+            const config = readJsonFile('config.json', 'config');
+
+            //console.log(req.query);
+            const data = config.find(object => {
+                const isPathAndMethodMatch = (object.path === req.path) && (object.method === req.method);
+                
+                // Simplified query comparison for demonstration
+                const isQueryMatch = Object.keys(object.query).every(key => key in req.query);
+                
+                return isPathAndMethodMatch && isQueryMatch;
+            });
+
+            if (data) {
+                let responseContent = readTemplateFile(data.file, 'responses');
+                
+                // Replace placeholders with query parameters
+                for (const key in req.query) {
+                    const placeholder = `{{${key}}}`;
+                    if (responseContent.includes(placeholder)) {
+                        responseContent = responseContent.replace(new RegExp(placeholder, 'g'), req.query[key]);
+                    }
+                }
+                
+                // Parse the final content and send the response
+                const jsonResponse = JSON.parse(responseContent);
+                return res.status(data.code).json(jsonResponse);
+            }
         }
     } catch(error) {
+        console.log(error);
         return res.status(500).json({
             message: "invalid url"
         });
